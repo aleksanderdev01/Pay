69c83bd931603279c3f93d0f2e04d192278d3c07
AlexZe_Dev <45917713+alexzedev@users.noreply.github.com>
2024-04-18 11:56:14 +0200

    program with polish language comments inside

---
+++ pay_pl.py
@@ -0,0 +1,8 @@
+#Pyta o liczbę przepracowanych godzin
+xh = input("Enter Hours: ")
+#Pyta o stawkę godzinową
+xr = input ("Enter Rate: ")
+#mnoży obie wartości wyrażone jako liczby zmiennoprzecinowe
+xp = float(xh) * float(xr)
+#Drukuje wyliczoną wartość wypłaty
+print("Pay:", xp)
